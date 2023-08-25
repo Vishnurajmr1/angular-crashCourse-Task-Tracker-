a46a480bcf04170fce20958c2238300a1aabc89c
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-08-25 14:14:47 +0530

    router completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component ,OnInit} from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
+import {Router} from '@angular/router'
 import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-header',
@@ -10,13 +11,17 @@ export class HeaderComponent implements OnInit {
   title:string = 'Task Tracker';
   showAddTask!: boolean;
   subscription!:Subscription;
-  constructor(private uiService:UiService){
+  constructor(private uiService:UiService,private router:Router){
     this.subscription=this.uiService.onToggle().subscribe(value=>this.showAddTask=value)
   }
   ngOnInit(): void {
   }
   toggleAddTask(): void{
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string):boolean{
+    return this.router.url===route;
   }
 }
 
