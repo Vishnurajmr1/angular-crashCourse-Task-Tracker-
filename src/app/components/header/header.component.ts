da104ed0c74cb5db74ed784438cf2588de076b5a
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-08-25 13:54:14 +0530

    task adding completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component ,OnInit} from '@angular/core';
-
+import { UiService } from 'src/app/services/ui.service';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title:string = 'Task Tracker';
-
-  toggleAddTask(){
-    console.log('Toggle');
-    
+  showAddTask!: boolean;
+  subscription!:Subscription;
+  constructor(private uiService:UiService){
+    this.subscription=this.uiService.onToggle().subscribe(value=>this.showAddTask=value)
+  }
+  ngOnInit(): void {
+  }
+  toggleAddTask(): void{
+    this.uiService.toggleAddTask();
   }
 }
 
